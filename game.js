e0f5c1e7989e93c627c414944f1127195507e902
Test <justtest@gmail.com>
2020-04-09 20:28:42 +0200

    Almost

--- game.js
+++ game.js
@@ -5,11 +5,49 @@
  let cards = document.querySelectorAll("div");
  cards = [...cards];
 
- const startTimer = new Date().getTime();
+ const startTime = new Date().getTime();
 
  let activeCard = "";
+ const activeCards = [];
 
- const clickCard = function(){}
+ const gamePairs = cards.lenght/2;
+ let gameResult = 0;
+
+ const clickCard = function(){
+    activeCard = this;
+    activeCard.classList.remove("hidden");
+
+    if(activeCards.length === 0){
+        activeCards[0] = activeCard;
+        return
+    }
+    else {
+        cards.forEach(card => {
+            card.removeEventListener("click", clickCard)
+        })
+        activeCards[1] = activeCard;
+
+        setTimeout(function (){
+
+        if(activeCards[0].className === activeCards[1].className){
+            activeCards.forEach(card => card.classList.add("off"))
+            if (gameResult == gamePairs){
+                const endTime = new Date().getTime();
+                const gameTime = (endTime - startTime)/1000;
+                alert(`Congratulations! Your Score is: ${gameTime} sekund`);
+                location.reload();
+            }
+        }
+        else{
+            activeCards.forEach(card => card.classList.add("hidden"))
+        }
+        activeCard = "";
+        activeCards.length = 0;
+        cards.forEach(card => card.addEventListener("click", clickCard))
+
+    }, 500)
+}
+ }
 
  const init = function() {
      cards.forEach(card =>{
